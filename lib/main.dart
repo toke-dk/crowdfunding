c22add037801cedac6fe91fcf44246bb1e1b4871
Toke Friis <tokefriis@gmail.com>
2024-03-19 20:50:34 +0100

    made title to the page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -10,11 +12,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        textTheme: GoogleFonts.montserratTextTheme().copyWith(displayLarge: GoogleFonts.montserrat(fontWeight: FontWeight.w700)),
+        textTheme: GoogleFonts.montserratTextTheme().copyWith(
+            displayMedium: GoogleFonts.montserrat(fontWeight: FontWeight.w700),
+        displayLarge: GoogleFonts.montserrat(fontWeight: FontWeight.w800, fontSize: 170),),
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
@@ -34,10 +37,34 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox(
-        width: 700,
-        child: Text(
-          'Farum Badmintons Indsamlingskapagne til Kina',style: Theme.of(context).textTheme.displayLarge, textAlign: TextAlign.center,
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 60,),
+            SizedBox(width: 400, height: 80, child: Placeholder()),
+            SizedBox(height: 50,),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Text(
+                'Støt unge badmintontalenter!'.toUpperCase(),
+                style: Theme.of(context).textTheme.displayMedium,
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            SizedBox(
+              width: 900,
+              child: Text(
+                "Ved at give økonomisk støtte til Farum Badminton, giver du muligheden for dygtige badmintonspillere i Farum Badminton om en livs rejse",
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.displaySmall!.copyWith(fontSize: 20),
+              ),
+            ),
+            SizedBox(height: 300,),
+            Text("200.000.-", style: Theme.of(context).textTheme.displayLarge,)
+          ],
         ),
       ),
     );
