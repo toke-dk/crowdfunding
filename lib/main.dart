dd585a9bdef918c511c39842b1414a26dcd85527
Toke Friis <tokefriis@gmail.com>
2024-03-19 20:57:23 +0100

    made the value animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:animated_flip_counter/animated_flip_counter.dart';
+import 'package:countup/countup.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -16,8 +19,10 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         textTheme: GoogleFonts.montserratTextTheme().copyWith(
-            displayMedium: GoogleFonts.montserrat(fontWeight: FontWeight.w700),
-        displayLarge: GoogleFonts.montserrat(fontWeight: FontWeight.w800, fontSize: 170),),
+          displayMedium: GoogleFonts.montserrat(fontWeight: FontWeight.w700),
+          displayLarge: GoogleFonts.montserrat(
+              fontWeight: FontWeight.w800, fontSize: 170),
+        ),
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
@@ -40,9 +45,13 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Column(
           children: [
-            SizedBox(height: 60,),
+            SizedBox(
+              height: 60,
+            ),
             SizedBox(width: 400, height: 80, child: Placeholder()),
-            SizedBox(height: 50,),
+            SizedBox(
+              height: 50,
+            ),
             Padding(
               padding: EdgeInsets.symmetric(horizontal: 10),
               child: Text(
@@ -59,11 +68,23 @@ class _MyHomePageState extends State<MyHomePage> {
               child: Text(
                 "Ved at give økonomisk støtte til Farum Badminton, giver du muligheden for dygtige badmintonspillere i Farum Badminton om en livs rejse",
                 textAlign: TextAlign.center,
-                style: Theme.of(context).textTheme.displaySmall!.copyWith(fontSize: 20),
+                style: Theme.of(context)
+                    .textTheme
+                    .displaySmall!
+                    .copyWith(fontSize: 20),
               ),
             ),
-            SizedBox(height: 300,),
-            Text("200.000.-", style: Theme.of(context).textTheme.displayLarge,)
+            SizedBox(
+              height: 300,
+            ),
+            Countup(
+              duration: 2.seconds,
+              separator: ".",
+              begin: 0,
+              end: 200000,
+              suffix: ".-",
+              style: Theme.of(context).textTheme.displayLarge,
+            )
           ],
         ),
       ),
