c152c0ec171048e1d3c317e290e54af3cd93ffad
Toke Friis <tokefriis@gmail.com>
2024-03-19 21:07:36 +0100

    making the header

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:animated_flip_counter/animated_flip_counter.dart';
 import 'package:countup/countup.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -42,50 +41,64 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 60,
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height * 0.8,
+          color: Colors.blue[800],
+          constraints: const BoxConstraints(minHeight: 1200),
+          child: Center(
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 60,
+                ),
+                const SizedBox(width: 400, height: 80, child: Placeholder()),
+                const SizedBox(
+                  height: 50,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Text(
+                    'Støt unge badmintontalenter!'.toUpperCase(),
+                    style: Theme.of(context)
+                        .textTheme
+                        .displayMedium!
+                        .copyWith(color: Colors.white),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                SizedBox(
+                  width: 900,
+                  child: Text(
+                    "Ved at give økonomisk støtte til Farum Badminton, giver du muligheden for dygtige badmintonspillere i Farum Badminton om en livs rejse",
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context)
+                        .textTheme
+                        .displaySmall!
+                        .copyWith(fontSize: 20, color: Colors.white),
+                  ),
+                ),
+                const Spacer(),
+                Countup(
+                  duration: 2.seconds,
+                  separator: ".",
+                  begin: 0,
+                  end: 200000,
+                  suffix: ".-",
+                  style: Theme.of(context)
+                      .textTheme
+                      .displayLarge!
+                      .copyWith(color: Colors.white),
+                ),
+                const Spacer(
+                  flex: 3,
+                )
+              ],
             ),
-            SizedBox(width: 400, height: 80, child: Placeholder()),
-            SizedBox(
-              height: 50,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 10),
-              child: Text(
-                'Støt unge badmintontalenter!'.toUpperCase(),
-                style: Theme.of(context).textTheme.displayMedium,
-                textAlign: TextAlign.center,
-              ),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            SizedBox(
-              width: 900,
-              child: Text(
-                "Ved at give økonomisk støtte til Farum Badminton, giver du muligheden for dygtige badmintonspillere i Farum Badminton om en livs rejse",
-                textAlign: TextAlign.center,
-                style: Theme.of(context)
-                    .textTheme
-                    .displaySmall!
-                    .copyWith(fontSize: 20),
-              ),
-            ),
-            SizedBox(
-              height: 300,
-            ),
-            Countup(
-              duration: 2.seconds,
-              separator: ".",
-              begin: 0,
-              end: 200000,
-              suffix: ".-",
-              style: Theme.of(context).textTheme.displayLarge,
-            )
-          ],
+          ),
         ),
       ),
     );
